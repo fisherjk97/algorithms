7fd64bc01a8d9744e4f20529228c5ec68b84bede
John Fisher <jkfisher97@comcast.net>
2019-07-29 07:55:08 -0600

    adding additional graph test cases

--- Trees and Graphs/GraphSearch.cs
+++ Trees and Graphs/GraphSearch.cs
@@ -126,25 +126,42 @@ namespace Algorithms.Graphs
     {
         public static void Test(){
 
-        var vertices = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var vertices = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             var edges = new[]{Tuple.Create(1,2), Tuple.Create(1,3),
                 Tuple.Create(2,4), Tuple.Create(3,5), Tuple.Create(3,6),
                 Tuple.Create(4,7), Tuple.Create(5,7), Tuple.Create(5,8),
                 Tuple.Create(5,6), Tuple.Create(8,9), Tuple.Create(9,10)};
 
             var graph = new Graph<int>(vertices, edges);
+
+
+            var vertices2 = new[] {1, 2, 3, 5, 7, 8};
+            var edges2 = new[]{Tuple.Create(1,2), Tuple.Create(2,3),
+                Tuple.Create(5,7), Tuple.Create(5,2), Tuple.Create(7,8)};
+
+            var graph2 = new Graph<int>(vertices2, edges2);
+
             var algorithms = new Algorithms();
 
+            //Console.WriteLine("Depth First Search");
+            //Console.WriteLine(string.Join(", ", algorithms.DFS(graph, 1)));
             Console.WriteLine("Depth First Search");
-            Console.WriteLine(string.Join(", ", algorithms.DFS(graph, 1)));
+            Console.WriteLine(string.Join(", ", algorithms.DFS(graph2, 5)));
             //# 1, 3, 6, 5, 8, 9, 10, 7, 4, 2
             Console.WriteLine("Breadth First Search");
-            Console.WriteLine(string.Join(", ", algorithms.BFS(graph, 1)));
-
+            //Console.WriteLine(string.Join(", ", algorithms.BFS(graph, 1)));
+            Console.WriteLine(string.Join(", ", algorithms.BFS(graph2, 5)));
 
             var startVertex = 1;
             var shortestPath = algorithms.ShortestPathFunction(graph, startVertex);
             foreach (var vertex in vertices)
+                Console.WriteLine("shortest path to {0,2}: {1}",
+                        vertex, string.Join(", ", shortestPath(vertex)));
+
+
+            var startVertex2 = 5;
+            var shortestPath2 = algorithms.ShortestPathFunction(graph2, startVertex2);
+            foreach (var vertex in vertices2)
                 Console.WriteLine("shortest path to {0,2}: {1}",
                         vertex, string.Join(", ", shortestPath(vertex)));
         }
